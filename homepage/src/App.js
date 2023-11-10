7400e01f5faa1db58851c90b6983f0819da5607c
Tristan Anderson <kingmonster2004@gmail.com>
2023-11-10 00:54:20 -0600

    cleanup

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -5,43 +5,13 @@ import News from './pages/News';
 import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="Header">
-        <div className= "Nav-Element">
-          <button>
-            Home
-          </button>
-          <button>
-            News
-          </button>
-          <button>
-            Portfolio
-          </button>
-          <button>
-            About
-          </button>
-        </div>
-        <div className="Nav-Element">
-          TRISTAN ANDERSON
-        </div>
-      </header>
-      <div className="Body">
-        <div className="Sidebar">
-
-        </div>
-        <div className="Content">
-
-        </div>
-      </div>
-      <footer className="Footer">
-
-      </footer> */}
-
       <Navbar></Navbar>
-      <div className='container'>
+      <div className='Body'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/portfolio' element={<Portfolio />} />
@@ -49,6 +19,7 @@ function App() {
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
