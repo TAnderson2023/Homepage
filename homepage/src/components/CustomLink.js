6b08a1cf75f033a7ac6c3b1173793e5370a92b2e
Tristan Anderson <kingmonster2004@gmail.com>
2023-11-14 22:52:19 -0600

    cleanup

--- homepage/src/components/CustomLink.js
+++ homepage/src/components/CustomLink.js
@@ -1,5 +1,4 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
 export default function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to);
     const isActive = useMatch({ path: resolvedPath.pathname, end: true })
